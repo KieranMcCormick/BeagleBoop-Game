b4699572772a5e32504b2d472b7a63ec2c977ad4
Jamie Epp <jaepp@sfu.ca>
2017-10-15 14:57:05 -0700

    Add potentiometer module

---
+++ Potentiometer.h
@@ -0,0 +1,17 @@
+/*
+ * Potentiometer.h
+ *
+ *  Created on: Oct 4, 2017
+ *      Author: jamie
+ */
+
+#ifndef POTENTIOMETER_H_
+#define POTENTIOMETER_H_
+
+void Potentiometer_init(void);
+
+void Potentiometer_cleanup(void);
+
+int Potentiometer_getValue(void);
+
+#endif /* POTENTIOMETER_H_ */
