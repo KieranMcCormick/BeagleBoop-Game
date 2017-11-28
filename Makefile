a9a8c893572bef878d0ea4adf1b45c942f084f4b
Kiearn McCormick <octavian13@shaw.ca>
2017-11-28 10:37:12 -0800

    magic

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 OUTFILE = gameTest
+OUTDIR = $(HOME)/cmpt433/public/myApps
 CROSS_COMPILE = arm-linux-gnueabihf-
 CC_C = $(CROSS_COMPILE)gcc
 
 CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror -pthread -I${CURDIR}/curl_bbg -I${CURDIR}/jsonc_bbg
 
-LFLAGS = -L${CURDIR}/curl_bbg/libs/ -L${CURDIR}/jsonc_bbg/libs/  -lcurl  -ljson-c
+LFLAGS = -L${CURDIR}/curl_bbg/libs/ -L${CURDIR}/jsonc_bbg/libs/ -L$(HOME)/cmpt433/public/asound_lib_BBB
 
 all:
-	$(CC_C) $(CFLAGS) main.c -o $(OUTFILE) $(LFLAGS)
+	$(CC_C) $(CFLAGS) main.c -o $(OUTDIR)/$(OUTFILE) $(LFLAGS) -lcurl  -ljson-c -lasound
--- main.c
+++ main.c
@@ -1,27 +1,60 @@
-//#include <json/json.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 #include <json/json.h>
+
+//https://beagle-boop.firebaseio.com/boards.json
 int main()
 {
-	CURL *curl;
-	CURLcode res;
+        CURL *curl;
+        CURLcode res;
 
-	curl = curl_easy_init();
-	if (curl)
-	{
-		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_global_init(CURL_GLOBAL_DEFAULT);
 
-		res = curl_easy_perform(curl);
-		if (res != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-					curl_easy_strerror(res));
+        curl = curl_easy_init();
+        if (curl)
+        {
+                curl_easy_setopt(curl, CURLOPT_URL, "https://beagle-boop.firebaseio.com/boards.json");
 
-		curl_easy_cleanup(curl);
-	}
+#ifdef SKIP_PEER_VERIFICATION
+                /*
+     * If you want to connect to a site who isn't using a certificate that is
+     * signed by one of the certs in the CA bundle you have, you can skip the
+     * verification of the server's certificate. This makes the connection
+     * A LOT LESS SECURE.
+     *
+     * If you have a CA cert for the server stored someplace else than in the
+     * default bundle, then the CURLOPT_CAPATH option might come handy for
+     * you.
+     */
+                curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+#endif
 
-        struct json_object *jobj;
+#ifdef SKIP_HOSTNAME_VERIFICATION
+                /*
+     * If the site you're connecting to uses a different host name that what
+     * they have mentioned in their server certificate's commonName (or
+     * subjectAltName) fields, libcurl will refuse to connect. You can skip
+     * this check, but this will make the connection less secure.
+     */
+                curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+#endif
+
+                /* Perform the request, res will get the return code */
+                res = curl_easy_perform(curl);
+                /* Check for errors */
+                if (res != CURLE_OK)
+                        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                                curl_easy_strerror(res));
+
+                /* always cleanup */
+                curl_easy_cleanup(curl);
+        }
+
+        curl_global_cleanup();
+
+        /*         struct json_object *jobj;
         char *str = "{ \"msg-type\": [ \"0xdeadbeef\", \"irc log\" ], \
                 \"msg-from\": { \"class\": \"soldier\", \"name\": \"Wixilav\" }, \
                 \"msg-to\": { \"class\": \"supreme-commander\", \"name\": \"[Redacted]\" }, \
@@ -36,7 +69,7 @@ int main()
         printf("str:\n---\n%s\n---\n\n", str);
 
         jobj = json_tokener_parse(str);
-        printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY));
+        printf("jobj from str:\n---\n%s\n---\n", json_object_to_json_string_ext(jobj, JSON_C_TO_STRING_SPACED | JSON_C_TO_STRING_PRETTY)); */
 
-	return 0;
+        return 0;
 }
